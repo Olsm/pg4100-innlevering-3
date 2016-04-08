9733e95cfa493d694f314f393342a0b37295398d
OlavOlsm <OlavOlsm@gmail.com>
2016-04-08 14:01:25 +0200

    Add isPrimeNumber function

--- src/main/java/com/smaola14/PrimeNumberCalculator.java
+++ src/main/java/com/smaola14/PrimeNumberCalculator.java
@@ -1,5 +1,10 @@
 package com.smaola14;
 
+import static org.apache.commons.math3.primes.Primes.isPrime;
+
 public class PrimeNumberCalculator {
 
+    public boolean isPrimeNumber(int n) {
+        return isPrime(n);
+    }
 }
